54f245861ba9692a6346c9d5aa05c5ba0a977214
Joshua Land <joshua.land6@gmail.com>
2023-04-27 07:31:34 -0700

    grows slowly changes colors, needs to be updated to entire background color not just a strip and maybe a box

--- simpleClock.js
+++ simpleClock.js
@@ -9,7 +9,35 @@ function Clock() {
 
 	// bundle it all in some json
 	let time = { "hour" : hour, "minute" : minutes, "seconds" : seconds }
+	console.log(time["seconds"]);
 	return  time;
 
 
+};
+
+function updateClock(){
+	// grab the current time in the form of JSON
+	let timeJSON = Clock();
+
+	// check the checkbox to see if its military time or not
+	let checkBox = document.getElementById('checkBox');
+	let standardTime = checkBox.checked
+	let hour; let amPm;
+	// extract time a little 12 hour conversion 
+	if (standardTime){
+		hour = (timeJSON['hour'] % 12).toString();
+		amPm = timeJSON["hour"] > 12 ? " : PM" : " : AM";
+	} else {
+		hour = timeJSON['hour'].toString();
+		 amPm = "";
+	};
+	// these are simpler
+	let minute = timeJSON["minute"].toString();
+	let seconds = timeJSON["seconds"].toString();
+	// this gets the og military time, to tell if its am, pm.
+	// get my clock holder
+	let divtext = document.getElementById("clockText");
+	// build a string.
+	let clockText = hour + " : " + minute + " : " + seconds + amPm
+	divtext.innerHTML = clockText;
 };
